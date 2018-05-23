2f6e996dbfdada2d1892445e5854bfabb445eefa
sergeome <sergeome@gmail.com>
2018-05-22 21:13:33 -0500

    add separate routs for demonstration

--- src/main/java/SparkResources.java
+++ src/main/java/SparkResources.java
@@ -2,6 +2,7 @@ import static spark.Spark.*;
 
 public class SparkResources {
     public static void defineResources() {
-        get("/", (req, res) -> "Hello World");
+        get("/", (req, res) -> "This is root page");
+        get("/example", (req, res) -> "This is example page");
     }
 }
